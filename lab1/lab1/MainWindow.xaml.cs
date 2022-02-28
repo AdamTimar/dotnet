4905b8d1bf802bf4bb2b09789ae44e6481e0ac7c
AdamTimar <adamscy15@gmail.com>
2022-02-28 10:14:45 +0200

    lab1

---
+++ lab1/lab1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Person person;
+        private List<Person> persons;
+        public MainWindow()
+        {
+            person = new Person();
+            persons = new List<Person>;
+            InitializeComponent();
+            runMeButton.IsEnabled = false;
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            person.FirstName = firstNameTextBox.Text;
+            person.LastName = lastNameTextBox.Text;
+            MessageBox.Show($"Hello {person.FirstName} {person.LastName}");
+            person = new Person();
+        }
+
+        private  void FirstNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (firstNameTextBox.Text.Length > 0 && lastNameTextBox.Text.Length > 0)
+                runMeButton.IsEnabled = true;
+            else
+                runMeButton.IsEnabled = false;
+
+        }
+
+        private void LastNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (firstNameTextBox.Text.Length > 0 && lastNameTextBox.Text.Length > 0)
+                runMeButton.IsEnabled = true;
+            else
+                runMeButton.IsEnabled = false;
+        }
+    }
+}
---
+++ lab2/lab2/Helper/HeaderToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Media.Imaging;
+using System.IO;
+namespace lab2.Helper
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter headerToImageConverter = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+            if (path == null)
+                return null;
+            string name = MainWindow.getFileFolderName(path);
+            string image = "Images/elso.jpg";
+            if (string.IsNullOrEmpty(name))
+                image = "Images/masodik.jpg";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/harmadik.jpg";
+            return new BitmapImage(new Uri($"pack://application:,,/{image}"));
+        }
+        public object ConvertBack(object value, Type targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
